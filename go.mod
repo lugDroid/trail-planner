3883422e49b0c107b2249a1639f0e05384c4cd15
lugDroid <diazbastos@gmail.com>
2022-07-15 12:28:54 +0200

    Add pq package and database connection code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lugdroid/trailPlanner/webapp
 
 go 1.18
+
+require github.com/lib/pq v1.10.6
--- src/main.go
+++ src/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"lugdroid/trailPlanner/webapp/controller"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	db := connectToDatabase()
+	defer db.Close()
+
 	var routesController controller.Routes
 	var uploadController controller.Upload
 
@@ -13,4 +21,18 @@ func main() {
 	uploadController.RegisterRoutes()
 
 	http.ListenAndServe(":3000", nil)
+}
+
+func connectToDatabase() *sql.DB {
+	db, err := sql.Open("postgres", os.Getenv("TP_DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Connection to database failed: ", err)
+	}
+
+	return db
 }
